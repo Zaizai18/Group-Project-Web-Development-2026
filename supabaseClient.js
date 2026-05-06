557014ffe79c937704826e8771b46bb5e423ef21
ryanlui13 <ryan.luigi.2020@gmail.com>
2026-05-06 18:02:07 -0400

    fixed sql link

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://dgctrgjdkhmxthkwhxyp.supabase.co';
 const supabaseAnonKey = 'sb_publishable_YSPCdd4cpBcaxsDFdJMbAg_EBXHOcTY';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
