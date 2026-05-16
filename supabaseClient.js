03b83b26d4440f98bbcd97fcfd45c5fc02e1456e
Zaizai18 <zainabshahzadi77@gmail.com>
2026-05-16 03:53:34 -0400

    changes

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dgctrgjdkhmxthkwhxyp.supabase.co';
-const supabaseAnonKey = 'sb_publishable_YSPCdd4cpBcaxsDFdJMbAg_EBXHOcTY';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
