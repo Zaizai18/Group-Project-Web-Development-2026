859a066c9ce2c293f063b0de3e13dfc07874b25d
Zaizai18 <zainabshahzadi77@gmail.com>
2026-05-16 04:24:55 -0400

    Deploying frontend codebase

--- Group-Project/vite.config.js
+++ Group-Project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
-  base: '/Web-Dev-React-Homework/', 
+  base: '/Group-Project-Web-Development-2026/', 
   plugins: [
     react(),
     tailwindcss(), 
